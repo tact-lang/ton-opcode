aeb57b2a6f2ca7eb45313070a5c5990217c5ab83
i582 <51853996+i582@users.noreply.github.com>
2025-01-16 16:49:15 +0400

    added 2 new opcodes for the new code

--- src/codepage/opcodes.gen.ts
+++ src/codepage/opcodes.gen.ts
@@ -106,8 +106,9 @@ export type OpCodeWithArgs =
     | { code: 'SAVE', args: [number] }
     | { code: 'SAVEALT', args: [number] }
     | { code: 'SAVEBOTH', args: [number] }
+    | { code: 'CALLDICT', args: [number] }
+    | { code: 'INLINECALLDICT', args: [number] }
     | { code: 'CALL', args: [number] }
-    | { code: 'CALLDICT'; args: [number] }
     | { code: 'JMP', args: [number] }
     | { code: 'PREPARE', args: [number] }
     | { code: 'THROW', args: [number] }
@@ -358,7 +359,6 @@ export type OpCodeNoArgs =
     | { code: 'RETFALSE' }
     | { code: 'RETBOOL' }
     | { code: 'CALLCC' }
-    | { code: 'INLINECALLDICT' }
     | { code: 'JMPXDATA' }
     | { code: 'CALLXVARARGS' }
     | { code: 'RETVARARGS' }
