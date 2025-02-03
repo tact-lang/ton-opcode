dfa0b5c55bec7f00c8e537a2453b89c5fd9eb45a
i582 <51853996+i582@users.noreply.github.com>
2025-02-03 11:15:10 +0400

    minor fixes

--- scripts/gen-codepage.js
+++ scripts/gen-codepage.js
@@ -2,5 +2,5 @@ let js2ts = require('json-schema-to-typescript')
 let fs = require('fs')
 
 // compile from file
-js2ts.compileFromFile(__dirname + '/../src/tvm-spec/schema.json')
+js2ts.compileFromFile(__dirname + '/../src/codepage/tvm-spec/schema.json')
   .then(ts => fs.writeFileSync(__dirname + '/../src/codepage/tvm-spec.d.ts', ts))
